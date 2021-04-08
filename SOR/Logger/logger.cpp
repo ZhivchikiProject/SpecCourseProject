d895e0fb56c5b4568278cbb1c6a75c3c0ea518d7
Ilya Mashkov <il.mashkov9@gmail.com>
2021-04-08 05:12:59 +0300

    [Logger] Adding logger to project
    How to use it is written in attached README

--- SOR/Logger/logger.cpp
+++ SOR/Logger/logger.cpp
@@ -1,16 +1,53 @@
 #include "logger.h"
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <sstream>
+#include <cassert>
+//#undef NDEBUG                  // uncomment to enable asserts in release build
 
 void DLL_EXPORT LOGGER::dll_load()
 {
     cout<<"LOGGER_LOAD\n";
 }
 
-
-
-
-
+void  DLL_EXPORT LOGGER::Logger::WriteLog(LogLevel logLevel, std::string message)
+{
+    std::ofstream logFile("Logfile.txt", std::ios::app);
+    if (logLevel == LogLevel::Info)
+    {
+        logFile << message << "\n";
+    }
+    if (logLevel == LogLevel::Warning)
+    {
+        logFile << "Warning: " << message << "\n";
+        std::cout << "Warning: " << message << "\n";
+    }
+    if (logLevel == LogLevel::Error)
+    {
+        logFile << "Error: " << message << "\n";
+        logFile.close();
+        std::cerr << "Error: " << message << "\n";
+        assert(0);
+    }
+    //if (logLevel == LogLevel::DEBUG) {}
+    logFile.close();
+}
+void DLL_EXPORT LOGGER::Logger::ShowLog()
+{
+    std::ifstream logFile("Logfile.txt");
+    if (logFile.is_open())
+    {
+        std::string tmp;
+        while (!logFile.eof())
+        {
+            std::getline(logFile, tmp);
+            std::cout << tmp << std::endl;
+        }
+        logFile.close();
+    }
+    else
+        std::cout << "Log file doesn't exist\n";
+}
 
 
 // a sample exported function
