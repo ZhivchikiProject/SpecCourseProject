c09fc045e21041e788b20789dec914b98f397527
Makcum888e <79456407+Makcum888e@users.noreply.github.com>
2021-04-09 21:14:43 +0300

    script v1.1

--- SOR/Console/bin/Debug/start.bat
+++ SOR/Console/bin/Debug/start.bat
@@ -2,9 +2,9 @@
 for /l %%i in (3,1,9) do (
 	for /l %%j in (0,1,2) do (
 		if exist C:/SOR/Tests/Results/ans%%i_%%j (
-			call Console C:/SOR/Tests/test%%i.1.txt C:/SOR/Tests/test%%i.2.txt %%j C:/SOR/Tests/ans%%i_%%j.txt C:/SOR/Tests/Results/ans%%i_%%j
+			call Console ../../../Tests/test%%i.1.txt ../../../Tests/test%%i.2.txt %%j ../../../Tests/ans%%i_%%j.txt ../../../Tests/Results/ans%%i_%%j
 		) else (
-			call Console C:/SOR/Tests/test%%i.1.txt C:/SOR/Tests/test%%i.2.txt %%j C:/SOR/Tests/ans%%i_%%j.txt
+			call Console ../../../Tests/test%%i.1.txt ../../../Tests/test%%i.2.txt %%j ../../../Tests/ans%%i_%%j.txt
 		)
 	)
 )
