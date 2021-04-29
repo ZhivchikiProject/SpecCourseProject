54a63152df0246a2bccf70035a7c2554ec25bb8b
Vladislav <31815075+vladkulikov@users.noreply.github.com>
2021-04-29 10:14:22 +0300

    Add files via upload

---
+++ SOR/Visualisator.v1/Visualisator.v1/Visualisato.v1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Visualisato.v1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(0, 62);
+			this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(65535, 65535);
+			this.panel1.TabIndex = 1;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(65535, 28);
+			this.menuStrip1.TabIndex = 2;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// openFileToolStripMenuItem
+			// 
+			this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+			this.openFileToolStripMenuItem.Size = new System.Drawing.Size(155, 26);
+			this.openFileToolStripMenuItem.Text = "Open File";
+			this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(16, 27);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 28);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Paint";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(124, 27);
+			this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(100, 28);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Clear";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Checked = true;
+			this.radioButton1.Location = new System.Drawing.Point(231, 31);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(55, 21);
+			this.radioButton1.TabIndex = 8;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Red";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(292, 31);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(69, 21);
+			this.radioButton2.TabIndex = 9;
+			this.radioButton2.Text = "Green";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(367, 31);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(57, 21);
+			this.radioButton3.TabIndex = 10;
+			this.radioButton3.Text = "Blue";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.ClientSize = new System.Drawing.Size(1924, 1055);
+			this.Controls.Add(this.radioButton3);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.MaximumSize = new System.Drawing.Size(999999, 999998);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton3;
+	}
+}
+
---
+++ SOR/Visualisator.v1/Visualisator.v1/Visualisato.v1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Visualisato.v1
+{
+	public partial class Form1 : Form
+	{
+        Bitmap bmp;
+        Graphics g;
+        string fname = @"C:\Users\vadya\OneDrive\Рабочий стол\test.txt";
+        public Form1()
+		{
+			InitializeComponent();
+
+        }
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+
+        }
+
+
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+ 
+        }
+
+		private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+            OpenFileDialog opfd = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fname = openFileDialog1.FileName;
+            }
+            
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+            Graphics g = Graphics.FromHwnd(panel1.Handle);
+
+            string[] strs = File.ReadAllText(fname).Split(',','\n', '\r',' ' , '\t');
+
+
+
+            List<List<int>> M = new List<List<int>>();
+            int k = 4;
+
+            Int32.TryParse(strs[1], out int a);
+            for (int i = 0; i < a; i++)
+            {
+                M.Add(new List<int>());
+            }
+            int count = 0;
+            for (int i = 0; i < M.Count; i++)
+            {
+                Int32.TryParse(strs[3 + count], out int value2);
+                for (int j = 0; j < value2 * 2; j++)
+                {
+                    Int32.TryParse(strs[k], out int value);
+                    M[i].Add(value / 100);
+                    k += 2;
+                }
+
+                count = M[i].Count * 2 + 1 + count;
+                k = count + 4;
+            }
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+			if (radioButton1.Checked)
+			{
+				pen = new Pen(Color.Red, 10);
+			}
+			else
+			{
+				if (radioButton2.Checked)
+				{
+					pen = new Pen(Color.Green, 7);
+				}
+				else
+				{
+					pen = new Pen(Color.Blue, 5);
+				}
+			}
+
+
+			for (int i = 0; i < M.Count; i++)
+            {
+                for (int j = 0; j < M[i].Count - 2; j += 2)
+                {
+                    g.DrawLine(pen, M[i][j], this.Height - M[i][j + 1], M[i][j + 2], this.Height - M[i][j + 3]);
+
+                }
+            }
+
+        }
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+            panel1.Invalidate();
+		}
+	}
+}
+
+
+
